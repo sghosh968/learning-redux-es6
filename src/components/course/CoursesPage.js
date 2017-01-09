da6c9893cf486128997f41bb9c339368c36a2f7b
Suman Ghosh <suman@sporkapps.com>
2017-01-09 18:36:39 +0530

    Completed adding layout/navigation & routing using react-router

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from '../App';
+import HomePage from '../home/HomePage';
+import AboutPage from '../about/AboutPage';
+
+
+class CoursesPage extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1> Courses list </h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
